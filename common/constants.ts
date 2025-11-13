054c433bdd78f462f3e61b2e794ca6c548beed3f
ivmerk <ivan.e.merkulov@gmail.com>
2025-11-13 12:16:57 +0200

    changing agent.conf name fixed

--- common/constants.ts
+++ common/constants.ts
@@ -3,7 +3,7 @@ export const PLUGIN_NAME = 'Scopd';
 export const CONFIGURATION_FILES_PATH = '/src/plugins/integrations/custom-configuration-files/';
 
 export const SCOPD_RULES_FILE_NAME = 'scopd_rules.xml';
-export const SCOPD_AGENT_CONF_FILE_NAME = 'agent.conf.xml';
+export const SCOPD_AGENT_CONF_FILE_NAME = 'agent.conf';
 export const SCOPD_DECODER_FILE_NAME = 'scopd_decoder.xml';
 export const SCOPD_OSSEC_CONF_FILE_NAME = 'ossec.conf.xml';
 
