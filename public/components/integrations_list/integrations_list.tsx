ceb01fb24fc71f29f720b6d2858ab65910d24b9c
ivmerk <ivan.e.merkulov@gmail.com>
2026-01-06 09:52:14 +0200

    small refuctoring done

--- public/components/integrations_list/integrations_list.tsx
+++ public/components/integrations_list/integrations_list.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
-import { EuiPanel, EuiButton, EuiHealth, EuiFlexGroup, EuiFlexItem } from '@elastic/eui';
+import {
+  EuiPanel,
+  EuiButton,
+  EuiHealth,
+  EuiFlexGroup,
+  EuiFlexItem } from '@elastic/eui';
 import { FormattedMessage } from '@osd/i18n/react';
 import './integrations.scss';
 import {IntegrationIcon} from "./integration_icon";
